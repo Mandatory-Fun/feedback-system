ee655e798f34a2858740d12ff9c4fde60fb32b28
HuyVo <huy.vminh71@gmail.com>
2021-10-19 16:38:37 +0300

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { initializeApp } from 'firebase/app';
 
+import { Provider } from 'react-redux';
+import makeStore from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
 try {
   const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE as string);
   initializeApp(firebaseConfig);
@@ -14,4 +18,13 @@ try {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={makeStore()}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
