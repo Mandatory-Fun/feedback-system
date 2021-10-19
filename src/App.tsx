f0a3b2b57b8341862539a53e608cf4113b599c31
HuyVo <huy.vminh71@gmail.com>
2021-10-19 16:36:49 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import LoginPage from './pages/login';
+import GuardRoute from './routes/GuardRoute';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+
+        <Route path="/login" component={LoginPage} />
+        <GuardRoute path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
